a1e8873a678fccfac19cb714bcdcddddda14b866
JerzhaLalu <158821591+JerzhaLalu@users.noreply.github.com>
2024-10-07 20:44:51 +0800

    Update Car.java

--- Exercise Set 2/Car.java
+++ Exercise Set 2/Car.java
@@ -4,7 +4,7 @@ class Car {
     private char size;
 
     public static void main(String[] args) {
-        Car car1 = new Car("red", 19999.85, 'm');
+        Car car1 = new Car("red", 19999.85, 'M');
         Car car2 = new Car("blue", 50000.00, 'L');
 
         System.out.println(car1);
