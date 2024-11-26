e607a30a22e6c7636e58f6b5b656e9aef8963496
JerzhaLalu <158821591+JerzhaLalu@users.noreply.github.com>
2024-11-26 20:15:01 +0800

    Add files via upload

---
+++ Laboratory Activity/Laboratory Activity 4 - 26-11-2024/Shapes/Rectangle.java
@@ -0,0 +1,29 @@
+public class Rectangle implements Shape {
+        
+        private double length;
+        private double width;
+    
+        public Rectangle(double length, double width) {
+            this.length = length;
+            this.width = width;
+        }
+    
+        @Override
+        public double getArea() {
+            return length * width; 
+        }
+        
+        @Override
+        public double getPerimeter() {
+            return length*2 + width*2; 
+        }
+    
+        @Override
+        public void printShapeType() {
+            System.out.println("Rectangle");
+        }
+    
+        
+    }
+
+
---
+++ Laboratory Activity/Laboratory Activity 4 - 26-11-2024/Shapes/Triangle.java
@@ -0,0 +1,29 @@
+public class Triangle implements Shape {
+    private double a;
+    private double b;
+    private double c;
+    
+    public Triangle(double a, double b, double c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+     }
+    
+    @Override
+    public double getArea() {
+        double s = (a + b + c) / 2;
+        return Math.sqrt(s * (s - a) * (s - b) * (s - c));
+    }
+        
+    @Override
+    public double getPerimeter() {
+        double P;
+        P = a + b + c;
+        return P; 
+    }
+    
+    @Override
+    public void printShapeType() {
+        System.out.println("Square");
+    }
+}
