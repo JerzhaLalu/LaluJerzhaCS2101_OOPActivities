9e559efac9b090b7bb8204ba76a9da3e1cca623c
JerzhaLalu <158821591+JerzhaLalu@users.noreply.github.com>
2024-11-26 23:14:52 +0800

    Add files via upload

---
+++ Laboratory Activity/Laboratory Activity 4 - 26-11-2024/Shape/ShapeCollection.java
@@ -0,0 +1,22 @@
+import java.text.DecimalFormat;
+
+public class ShapeCollection {
+    
+    private Shape[] shapes;
+
+    public ShapeCollection(Shape[] shapes) {
+        this.shapes = shapes;
+    }
+    
+    public void getPropertyValues() {
+        DecimalFormat df = new DecimalFormat("#.00"); 
+        for (Shape shape : shapes) {
+            System.out.println("Shape Type: " + shape.printShapeType());
+            System.out.println("Area: " + df.format(shape.getArea()));
+            System.out.println("Perimeter: " + df.format(shape.getPerimeter()));
+            System.out.println(); 
+        }
+    }
+    
+}
+
